7ae9a4299a9de352abd10a8959e66fc57c644d33
Luiz Fernando <ssluizf@gmail.com>
2021-04-23 22:17:48 -0300

    Quiz new architecture

---
+++ aps/java/lpoo/APSJAVALPOO.java
@@ -0,0 +1,62 @@
+package aps.java.lpoo;
+
+import java.util.Scanner ;
+
+
+class Energia {
+    
+   
+}
+
+
+class energianuclear extends Energia {
+    
+    
+}
+
+class Main {
+    public static void main (String [] args) {
+
+        String q1 = "Digite o tema desejado: \n" 
+            + "1 - Fontes Renovaveis\n2 - Lixo \n3 - Impactos Humanos \n4 - Aquecimento Global \n5 - Queimadas\n";
+
+        String q2 = "Digite o tema desejado: \n" 
+            + "1 - Energia Eólica\n2 - Biomassa \n3 - Energia Geotérmica \n4 - Energia Oceânica \n5 - Energia Solar\n";
+
+        String q3 = "Digite o tema desejado: \n" 
+            + "1 - Beneficios\n2 - Desvantagens \n3 - Custo Beneficio \n4 - Curiosidades \n5 - N'ao sei \n0";
+
+        Question [] questions = {
+            new Question("", ""), // Go back handling
+            new Question(q1, "1"),
+            new Question(q2, "1"),
+            new Question(q3, "1"),
+        };
+
+        createTest(questions);
+    }
+
+    public static void createTest(Question [] questions) {
+        int score = questions.length - 1;
+        Scanner keyboardInput = new Scanner(System.in);
+
+        for(int i = 1; i < questions.length; i++) {
+            System.out.println(questions[i].prompt);
+            if(i != 1) {
+                System.out.println("0 - Voltar");
+            }
+
+            String answer = keyboardInput.nextLine();
+
+            if(i != 1 && answer.equals("0")) {
+                i -= 2;
+            }
+
+            if(answer.equals(questions[i].answer)) {
+                score++;
+            }
+        }
+        System.out.println("Sua pontuação foi " + score + "/" + (questions.length - 1));
+    }
+}
+
